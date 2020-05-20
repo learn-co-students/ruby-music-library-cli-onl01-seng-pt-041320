07507f61c92d7cb508f50d38d3c24b4f1a4f3983
vedransehovic <vedran.sehovic@gmail.com>
2020-05-20 17:12:08 -0400

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -3,11 +3,12 @@ class MusicLibraryController
 
     def initialize (path='./db/mp3s')
         @path = path
-        new_import = MusicImporter.new(@path)
-        new_import.import
+        new_import = MusicImporter.new(@path).import
     end
 
     def call
+        user_response = ""
+        while user_response != "exit"
         puts "Welcome to your music library!"
         puts "To list all of your songs, enter 'list songs'."
         puts "To list all of the artists in your library, enter 'list artists'."
@@ -17,9 +18,21 @@ class MusicLibraryController
         puts "To play a song, enter 'play song'."
         puts "To quit, type 'exit'."
         puts "What would you like to do?"
-        user_response = gets
-        while user_response != "exit"
-            user_response = gets
+        user_response = gets.strip
+            case user_response
+            when "list songs"
+                list_songs
+            when "list artists"
+                list_artists
+            when "list genres"
+                list_genres
+            when "list artist"
+                list_songs_by_artist
+            when "list genre"
+                list_songs_by_genre
+            when "play song"
+                play_song
+            end
         end
     end
 
@@ -75,10 +88,11 @@ class MusicLibraryController
 
     def play_song
         puts "Which song number would you like to play?"
-        song_number = gets
-        index = song_number.to_i - 1
-       # array_index = song_number - 1
-        puts "Playing song #{list_songs[song_number.to_i].name}" 
-    end
-
+        input = gets.to_i
+        if input > 0 && input <= Song.all.length
+          array = Song.all.sort{|a, b| a.name <=> b.name}
+          song = array[input-1]
+          puts "Playing #{song.name} by #{song.artist.name}"
+        end
+      end
 end
