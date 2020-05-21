983482006d154bf572ad11f7d97c94af43615d95
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-21 03:01:55 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,34 @@
 require 'bundler'
 Bundler.require
 
-module Concerns
-end
+
+module Concerns::Findable
+  def initialize(name)
+    #memorable instance
+    @name = name
+  end
+  
+  def all
+    #memorable class
+    @@all
+  end
+  
+  def destroy_all
+    #memorable class
+    self.all.clear
+  end
+  
+  def save
+    #memorable class
+    @@all << self
+  end
+  
+  def create(name)
+    #memorable class
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
 
 require_all 'lib'
