17fc0211bd10056faedb0386f12332ba6de99a62
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-20 20:23:47 -0700

    Done.

---
+++ lib/music_importer.rb
@@ -0,0 +1,23 @@
+class MusicImporter
+
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        files = []
+        Dir.new(self.path).each do |file|
+            files << file if file.length > 4
+        end
+        files
+    end
+
+    def import
+        self.files.each do |file_name|
+            Song.create_from_filename(file_name)
+        end
+    end
+
+end
--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,22 @@ class Song
         end
     end
 
+    def self.new_from_filename(file)
+        song_info = file.chomp(".mp3").split(" - ")
+        song = Song.find_or_create_by_name(song_info[1])
+        genre = Genre.find_or_create_by_name(song_info[2])
+        artist = Artist.find_or_create_by_name(song_info[0])
+        song.genre = genre
+        song.artist = artist
+        song
+    end
+
+    def self.create_from_filename(file)
+        file_name = Song.new_from_filename(file)
+        file_name.save
+        file_name
+    end
+
     #Associations
 
     def artist=(artist) 
