d9847a7b5936b6ec220ca13efd30c56c550b3805
Joseph Romero <joeromero47@gmail.com>
2020-05-22 20:44:51 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,46 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @songs = []
+    save 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.create(artist)
+    artist = self.new(artist)
+    artist.save 
+    artist 
+  end 
+  
+  def songs
+    @songs 
+  end 
+  
+  def add_song(song)
+    if song.artist != self 
+      song.artist = self 
+    end 
+    if !@songs.include?(song)
+      @songs << song 
+    end 
+      
+  end 
+  def genres 
+    songs.map(&:genre).uniq
+  end
+  
+end 
---
+++ lib/Song.rb
@@ -0,0 +1,52 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@all = []
+  def initialize(name, artist = nil, genre = nil)
+    @name = name 
+    if artist != nil 
+      self.artist = artist 
+    end 
+    if genre != nil 
+      self.genre = genre 
+    end 
+    save
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.create(song)
+    song = self.new(song)
+    song.save 
+    song 
+  end 
+  
+  def artist=(artist)
+   @artist = artist 
+   artist.add_song(self)
+  end 
+  
+ def genre=(genre)
+   @genre = genre 
+   genre.add_song(self)
+  end 
+  
+  def self.find_by_name(name)
+    all.find{ |song| song.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end 
+    
+end 
