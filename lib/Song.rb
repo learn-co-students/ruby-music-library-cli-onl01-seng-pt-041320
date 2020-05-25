6e56bfa42ba10ad03b521a640079efb046335130
Joseph Romero <joeromero47@gmail.com>
2020-05-25 00:04:14 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -10,7 +10,6 @@ class Song
     if genre != nil 
       self.genre = genre 
     end 
-    save
   end 
   
   def self.all 
@@ -48,5 +47,16 @@ class Song
   def self.find_or_create_by_name(name)
     find_by_name(name) || create(name)
   end 
+  
+  def self.new_from_filename(name)
+    artist, song, genre_name = name.split(' - ') 
+    fixed_name = genre_name.gsub('.mp3', '')
+    artist = Artist.find_or_create_by_name(artist)
+    genre = Genre.find_or_create_by_name(fixed_name)
+    new(song, artist, genre)
+  end 
     
+  def self.create_from_filename(name) 
+    new_from_filename(name).save
+  end 
 end 
