73b986d6b5db8bd2b88fec8ef9f6892c049caa41
timothyphilip <philippetimothe@gmail.com>
2020-05-25 01:27:25 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,28 +1,48 @@
 require 'pry'
 class MusicLibraryController
-    attr_accessor :path, :music, list_songs
+    attr_accessor :path, :music
 
     def initialize(path = './db/mp3s')
         @path = path
         @music = MusicImporter.new(path)
-        music.import
+        @music.import
     end
 
     def call
-        loop do 
+        input = ""
+        until input == "exit" 
             puts "Welcome to your music library!"
-            puts "To list all of your songs, enter 'list songs'."
+            puts "To list all of your songs, enter 'list songs'."  
+                if input == 'list songs'
+                    list_songs
+                end   
             puts "To list all of the artists in your library, enter 'list artists'."
+                if input == 'list artists'
+                    list_artists
+                end
             puts "To list all of the genres in your library, enter 'list genres'."
+                if input == 'list genres'
+                    list_genres
+                 end
             puts "To list all of the songs by a particular artist, enter 'list artist'."
+                if input == 'list artist'
+                    list_songs_by_artist
+                end
             puts "To list all of the songs of a particular genre, enter 'list genre'."
+                if input == 'list genre'
+                    list_songs_by_genre
+                end
             puts "To play a song, enter 'play song'."
+                if input == 'play song'
+                    play_song
+                end
             puts "To quit, type 'exit'."
             puts "What would you like to do?"
             input = gets.chomp
-            break if input == "exit"
         end 
-  #      binding.pry
+        
+        
+  
     end 
 
     def list_songs
@@ -82,3 +102,4 @@ class MusicLibraryController
 
 
 end 
+
