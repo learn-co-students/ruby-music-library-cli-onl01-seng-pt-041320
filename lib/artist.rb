7ab658f3d6d7e68702615b37990a521db449525f
eulis01 <eulis01@gmail.com>
2020-06-12 22:43:38 -0400

    added methods and comments

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,42 @@
 # frozen_string_literal: true
 
 class Artist
+  attr_accessor :name
+  extend Concerns::Findable # add extend to any class to store all the codes from different files into a module. passing all the method and instances from all classes to a module so they're all available thrught the whole cli.
+
+  @@all = []
+
+  def initialize(name) # initialized the name instance
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all 
+    @@all.clear   # remove all instances from array.
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(Song)
+    if !song.artist   #=> ! negation if the instance is not == to the instance itself then unique array made with the new instance.
+      song.artist = self 
+    end
+    self.song.map(&:genre).uniq # call the class itself on the song instance and map it out by genre and return a unique array.
+  end
 end
