b578ee6c98ec202bd53757c8e62d98c8322b683d
eulis01 <eulis01@gmail.com>
2020-06-13 18:51:50 -0400

    added song from file names instances

--- lib/song.rb
+++ lib/song.rb
@@ -11,23 +11,56 @@ class Song
     self.genre = genre if genre
   end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def save
     @@all << self
   end
 
-  def self.destroy_all 
+  def self.destroy_all
     @@all.clear
   end
 
   def self.create(name)
-    song =self.new(name)
+    song = new(name)
     song.save
     song
   end
 
-  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
   end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if !
+      find_by_name(name)
+      Song.create(name)
+    else
+      find_by_name(name)
+    end
+  end
+
+  def self.new_from_filename(file_name)
+    imported_song = file_name.split(" - ")[1]
+    imported_artist = file_name.split(" - ")[0]
+    imported_genre = file_name.split(" - ")[2].chomp(".mp3")
+
+    artist = Artist.find_or_create_by_name(imported_artist)
+    genre = Genre.find_or_create_by_name(imported_genre)
+    Song.new(imported_song, artist, genre)
+  end
+
+  
+end
