45c9f41277a19b46e752a7348f25e30f30b7d97a
eulis01 <eulis01@gmail.com>
2020-06-13 16:26:02 -0400

    added missing end

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,9 +2,10 @@
 
 module Concerns::Findable
   def find_by_name(name)
-  all.detect do |instance|
+    all.detect do |instance|
       instance.name == name
     end
+  end
 
   def find_or_create_by_name(name)
     !find_by_name(name) ? create(name) : find_by_name(name)
