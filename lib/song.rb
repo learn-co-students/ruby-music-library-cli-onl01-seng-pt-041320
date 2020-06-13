e2414dbdcccf385939101fe2d80bdcf66cc49529
eulis01 <eulis01@gmail.com>
2020-06-13 18:36:01 -0400

    initialized artist an genre

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Song
-  
+  attr_accessor :name
+  attr_reader :artist, :genre
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
 end
