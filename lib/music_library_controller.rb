38b0c5b8d8c77e7d0d12559661d90c6a4d3a8577
eulis01 <eulis01@gmail.com>
2020-06-13 16:26:41 -0400

    test

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class MusicLibraryController
+  attr_accessor :path
+
+  def initialize(path = './db/mp3s')
+    @path = path
+    MusicImporter.new(path).import
+  end
 end
