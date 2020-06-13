611080b84c29eebbcedc40662b330a683662a014
eulis01 <eulis01@gmail.com>
2020-06-13 18:29:16 -0400

    add user imput classes

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -53,9 +53,45 @@ class MusicLibraryController
   end
 
   def list_artists
-    # need to sort each artisrs by name
+    # need to sort each artists by name
     sort(Artist.all).each_with_index do |artist, i|
       puts "#{i + 1}. #{artist.name}"
     end
   end
+
+  def list_genres
+    # sort each genres by name
+    sort(Genres.all).each_with_index do |genre, i|
+      puts "#{i + 1}. #{genre.name}"
+    end
+  end
+
+  def sort(list)
+    # abstract the #sort mehod
+    list.sort {|a, b} a.name <=> b.name
+  end
+
+  def list_songs_by_artist 
+    # asking the user to input an artist name 
+    puts "Please enter the name of an artist:"
+    artist = gets.strip 
+    # Print a list of artist songs, alphabetized by tittle.
+    artists =  Song.all.select! {|song| song.artist.name == artist}
+    sort(artists).each_with_index do |song, i| 
+      puts "#{i + 1}. #{song.name} - #{song.genre.name}"
+    end
+  end
+
+  def list_songs_by_genre 
+    # ask the user to input genre name 
+    puts "Please enter the name of a genre:"
+    genre = gets.strip 
+    # Printing a list of genre's songs, alphabetized by title
+    genres = Song.all.select! {|song| song.genre.name  == genre}
+    sort(genres).each_with_index do |song, i|
+      puts "#{i + 1}. #{song.artist.name} - #{song.name}"
+    end
+  end
+
+  
 end
