e746c33d4eb71051f577d3efc680da27b57f467a
eulis01 <eulis01@gmail.com>
2020-06-12 23:16:11 -0400

    genre Class & module linked

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
 class Genre
+  attr_accessor :name
+  extend Concerns::Findable
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @song = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+
+  attr_reader :songs
+
+  def artist
+    songs.map(&:artist) .uniq
+    end
 end
