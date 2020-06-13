057a9ec9656b023eeb1012194886e8fba31b077a
eulis01 <eulis01@gmail.com>
2020-06-12 23:07:21 -0400

    added comments

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,24 +19,20 @@ class Artist
     @@all << self
   end
 
-  def self.destroy_all 
-    @@all.clear   # remove all instances from array.
+  def self.destroy_all
+    @@all.clear # remove all instances from array.
   end
 
   def self.create(name)
-    artist = self.new(name)
+    artist = new(name)
     artist.save
     artist
   end
 
-  def songs
-    @songs
-  end
-
-  def add_song(Song)
-    if !song.artist   #=> ! negation if the instance is not == to the instance itself then unique array made with the new instance.
-      song.artist = self 
-    end
+  def add_song(song)
+    unless song.artist
+      song.artist = self
+    end #=> ! negation if the instance is not == to the instance itself then unique array made with the new instance.
     self.song.map(&:genre).uniq # call the class itself on the song instance and map it out by genre and return a unique array.
   end
 end
